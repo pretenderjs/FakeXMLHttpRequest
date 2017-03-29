0c72a6094f4aaa3cb688a2f11878cbaa924a8e4a
Sebastian Helbig <sebastian@ro14.sh>
2017-03-29 15:44:14 +0200

    check for lowercase content-type header

--- src/fake-xml-http-request.js
+++ src/fake-xml-http-request.js
@@ -266,7 +266,7 @@ var FakeXMLHttpRequestProto = {
     verifyState(this);
 
     if (!/^(get|head)$/i.test(this.method)) {
-      if (!this.requestHeaders["Content-Type"] && !(data || '').toString().match('FormData')) {
+      if (!this.requestHeaders["Content-Type"] && !this.requestHeaders["content-type"] && !(data || '').toString().match('FormData')) {
         this.requestHeaders["Content-Type"] = "text/plain;charset=UTF-8";
       }
 
