d620a034f86a66d35408d075de241c8d6e69ce89
Matthew Dahl <mdahl@ciena.com>
2017-04-19 11:17:58 -0700

    Add failing tests

--- test/send_test.js
+++ test/send_test.js
@@ -15,12 +15,32 @@ test("sets Content-Type header for non-GET/HEAD requests if not set", function()
         "sets content-type when not set");
 });
 
-test("does not change Content-Type if explicitly set for non-GET/HEAD", function(){
+test("does not change Content-Type if explicitly set for non-GET/HEAD using key Content-TYpe", function(){
   xhr.open("POST", "/");
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send("data");
   equal(xhr.requestHeaders["Content-Type"], "application/json",
         "does not change existing content-type header");
+});
+
+test("does not set Content-Type if content-type explicitly set for non-GET/HEAD", function(){
+  xhr.open("POST", "/");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.send("data");
+  equal(xhr.requestHeaders["content-type"], "application/json",
+        "does not change existing content-type header");
+  equal(xhr.requestHeaders["Content-Type"], undefined,
+        "does not add Content-Type header");
+});
+
+test("does not set Content-Type if CoNtEnT-tYpE explicitly set for non-GET/HEAD", function(){
+  xhr.open("POST", "/");
+  xhr.setRequestHeader("CoNtEnT-tYpE", "application/json");
+  xhr.send("data");
+  equal(xhr.requestHeaders["CoNtEnT-tYpE"], "application/json",
+        "does not change existing content-type header");
+  equal(xhr.requestHeaders["CoNtEnT-tYpE"], undefined,
+        "does not add Content-Type header");
 });
 
 test("does not set Content-Type if data is FormData object", function(){
